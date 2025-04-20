468f987f08dc84e36c25938e3d070ddd5237b42a
zoli0829 <zoli0829@gmail.com>
2025-04-20 08:51:36 +0100

    finished day 49
    yesterday should have been day 48, my bad

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -8,14 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var username: String = ""
+    @State private var email: String = ""
+    
+    var disableForm: Bool {
+        username.count < 5 || email.count < 5
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Form {
+            Section {
+                TextField("Username", text: $username)
+                TextField("Email", text: $email)
+            }
+            
+            Section {
+                Button("Create Account") {
+                    print("Creating account...")
+                }
+            }
+            .disabled(disableForm)
         }
-        .padding()
     }
 }
 
